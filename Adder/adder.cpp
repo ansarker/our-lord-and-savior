538c907866ac8f8ee0e6432a5449ec48566430fe
ansarker <anissarker603@gmail.com>
2025-05-12 00:31:55 +0600

    add submodule

--- Adder/adder.cpp
+++ Adder/adder.cpp
@@ -1,9 +1,11 @@
 #include "adder.h"
 
-int mearlymath::add(int a, int b) {
-    return (a+b);
+double add(double a, double b)
+{
+  return (a + b);
 }
 
-float mearlymath::add(float a, float b) {
-    return (a+b);
+float add(float a, float b)
+{
+  return (a + b);
 }
--- Adder/adder.h
+++ Adder/adder.h
@@ -1,6 +1,5 @@
 #pragma once
 
-namespace mearlymath {
-    int add(int a, int b);
-    float add(float a, float b);
-}
+double add(double a, double b);
+
+float add(float a, float b);
