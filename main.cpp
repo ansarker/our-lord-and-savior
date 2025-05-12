c15e510cc011c7e1004416bac6b4451611dbafa3
ansarker <anissarker603@gmail.com>
2025-05-12 17:16:39 +0600

    Versioning cmake project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <adder.h>
 #include <GLFW/glfw3.h>
+#include <OLASConfig.h>
 
-int main() {
+int main(int argc, char* argv[]) {
 	std::cout << "Hey, bow down to our lord and savior\n";
-
 	std::cout << add(72.1f, 73.8f) << '\n';
+
+    std::cout << argv[0] << " Version " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << '\n';
 
 	GLFWwindow *window;
     if( !glfwInit() )
